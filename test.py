9a393a1c28f139872f66c91e47363e92f1281dc0
Sudhir Hasbe <shasbe@gmail.com>
2023-06-28 19:36:24 -0700

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import numpy as np
 st.title('Uber pickups in sea')
 
 # initiate the MotherDuck connection through a service token through
-con = duckdb.connect('md:?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoic2hhc2JlLmdtYWlsLmNvbSIsImVtYWlsIjoic2hhc2JlQGdtYWlsLmNvbSIsInVzZXJJZCI6ImU4NzhlMTJjLWZiYzAtNDJiMS05NjhlLWU3ZmJjMTk2NjQzYyIsImlhdCI6MTY4ODAwMzg2MSwiZXhwIjoxNzE5NTYxNDYxfQ.PTwhFvOt9h88AP7EzgU1g1ZidzBy-_Av0ZBXybSzflU')
+#con = duckdb.connect('md:?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoic2hhc2JlLmdtYWlsLmNvbSIsImVtYWlsIjoic2hhc2JlQGdtYWlsLmNvbSIsInVzZXJJZCI6ImU4NzhlMTJjLWZiYzAtNDJiMS05NjhlLWU3ZmJjMTk2NjQzYyIsImlhdCI6MTY4ODAwMzg2MSwiZXhwIjoxNzE5NTYxNDYxfQ.PTwhFvOt9h88AP7EzgU1g1ZidzBy-_Av0ZBXybSzflU')
 
 # connect to your MotherDuck database through 'md:mydatabase' or 'motherduck:mydatabase'
 # if the database doesn't exist, MotherDuck creates it when you connect
@@ -14,4 +14,4 @@ con = duckdb.connect('md:?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9
 
 # run a query to check verify that you are connected
 con.sql("SHOW DATABASES").show()
-con.sql("SELECT * FROM 's3://sudsomni1/sales_demo.csv'").show()
+#con.sql("SELECT * FROM 's3://sudsomni1/sales_demo.csv'").show()
