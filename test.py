8a67a08a8a595332482660b633de29590d8a9a2a
Sudhir Hasbe <shasbe@gmail.com>
2023-06-28 20:06:31 -0700

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ st.title('Sales Data from S3')
 conn = duckdb.connect('md:?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoic2hhc2JlLmdtYWlsLmNvbSIsImVtYWlsIjoic2hhc2JlQGdtYWlsLmNvbSIsInVzZXJJZCI6ImU4NzhlMTJjLWZiYzAtNDJiMS05NjhlLWU3ZmJjMTk2NjQzYyIsImlhdCI6MTY4ODAwMzg2MSwiZXhwIjoxNzE5NTYxNDYxfQ.PTwhFvOt9h88AP7EzgU1g1ZidzBy-_Av0ZBXybSzflU')
 
 # Perform a SELECT query
-#result = conn.execute("SELECT * FROM employees")
 result = conn.execute("SELECT * FROM 's3://sudsomni1/sales_demo.csv'").df()
 
 # Fetch all rows from the result set
